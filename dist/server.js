23b94d579ad24d74ec1d71294ec76e755707410a
Mario Giugno <mariogiugno@gmx.de>
2017-01-12 20:40:37 +0100

    Added some comments to server.js

--- dist/server.js
+++ dist/server.js
@@ -1,19 +1,47 @@
+/**
+ * The frontend is provided by a node express server.
+ * It serves the files (html, js, images, etc) and 
+ * redirects the calls to the configured backend.
+ */
 'use strict';
 
+console.log('Starting brain.dmp frontend node server.')
 var express = require('express');
 var app     = express();
 
+var port = process.env.PORT || 4712;
+var apiBaseUri = process.env.APIBASEURI || 'http://localhost:'+port;
+
+// Send headers
+/**
+ * Intercept calls and append header.
+ */
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
+// Serve content from public/static under /static
 app.use('/static', express.static('public/static'));
 
+// Serve index.html
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// API SECTION
+/**
+ * Manage (redirect) frontend calls to api.
+ */
 app.get('/api/v1/', function(req, res) {
     res.send('Simple Server!');
 });
 
-var port = process.env.PORT || 4712;
+
+// Start
 app.listen(port, function() {
     console.log('Listening on port ' + port);
 });
