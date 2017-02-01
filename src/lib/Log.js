1f5367e8fbd7514f3b166ae0ee7255beda061bf9
Mario Giugno <mariogiugno@gmx.de>
2017-02-01 22:18:56 +0100

    Added Logger with conditional logging functions

---
+++ src/lib/Log.js
@@ -0,0 +1,91 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE_Logger in the project root for license information.
+// ORIGINAL: https://github.com/IdentityModel/oidc-client-js (found no other source)
+
+// Modified by Mario Giugno
+
+let nopLogger = {
+    info(){},
+    warn(){},
+    error(){}
+};
+
+const NONE = 0;
+const ERROR = 1;
+const WARN = 2;
+const INFO = 3;
+
+let logger;
+let level;
+
+export default class Log {
+    static get NONE() {return NONE};
+    static get ERROR() {return ERROR};
+    static get WARN() {return WARN};
+    static get INFO() {return INFO};
+    
+    static reset(){
+        level = INFO;
+        logger = nopLogger;
+    }
+    
+    static get level(){
+        return level;
+    }
+    static set level(value){
+        if (NONE <= value && value <= INFO){
+            level = value;
+        }
+        else {
+            throw new Error("Invalid log level");
+        }
+    }
+    
+    static get logger(){
+        return logger;
+    }
+    static set logger(value){
+        if (value.info && value.warn && value.error){
+            logger = value;
+        }
+        else {
+            throw new Error("Invalid logger");
+        }
+    }
+    
+    static info(...args){
+        if (level >= INFO){
+            logger.info.apply(logger, Array.from(args));
+        }
+    }
+    static warn(...args){
+        if (level >= WARN){
+            logger.warn.apply(logger, Array.from(args));
+        }
+    }
+    static error(...args){
+        if (level >= ERROR){
+            logger.error.apply(logger, Array.from(args));
+        }
+    }
+
+    /* Modified (added) */
+    static infoIf(condition, args){
+        if(condition){
+            Log.info(args);
+        }
+    }
+    static warnIf(condition, args){
+        if(condition){
+            Log.warn(args);
+        }
+    }
+    static errorIf(condition, args){
+        if(condition){
+            Log.error(args);
+        }
+    }
+    /* Mod end */
+}
+
+Log.reset();
--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import Routes from './routes';
 
+if(LOG_ENABLE){
+    const Log = require('./lib/Log').default;
+    Log.logger = console;
+    Log.level = LOG_LEVEL || Log.INFO;
+    Log.info(`Logger startet. It works! Log.level=${Log.level}`);
+}
+
 ReactDOM.render(
     <Provider store={store}>
         <Routes />
