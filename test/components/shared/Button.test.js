336d29ce6414a260d16dca1719e1304d3d29a342
Mario Giugno <mariogiugno@gmx.de>
2017-03-07 18:10:54 +0100

    Added Button component test

--- test/components/shared/Button.test.js
+++ test/components/shared/Button.test.js
@@ -4,13 +4,26 @@ import Button from './../../../src/components/shared/Button';
 describe('<Button /> component', () => {
 
     let enzymeWrapper;
+    let props;
 
     beforeEach(() => {
-        enzymeWrapper = shallow(<Button />);
+        props = {
+            onClick: expect.createSpy()
+        };
+        enzymeWrapper = shallow(<Button {...props}/>);
+    });
+
+    afterEach(() => {
+        expect.restoreSpies();
     });
 
     it('should display correctly', () => {
         expect(enzymeWrapper.find('.button').exists()).toBeTruthy();
         expect(enzymeWrapper.find('button').exists()).toBeTruthy();
+    });
+
+    it('should call callback function on click', () => {
+        enzymeWrapper.find('button').simulate('click');
+        expect(props.onClick).toHaveBeenCalled();
     })
 });
